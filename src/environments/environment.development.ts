c3503bbbc6cd3810390e168c32de81cf4611cf9b
Carlos Rodriguez <carlosrodriguez@192.168.1.82>
2025-12-19 12:38:48 -0600

    Fix MySQL connection: use internal hostname and correct credentials (mysql user)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     appTitle: "reloj checador",
-    API_ROUTE: "http://localhost:5182/api/",
+    API_ROUTE: "http://144.217.167.101:7676/api/",
     tableLIMIT: 5
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    appTitle: "reloj checador",
+    API_ROUTE: "http://144.217.167.101:7676/api/",
+    tableLIMIT: 5
+};
