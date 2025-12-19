6c419ef96cebdf1cc0b7d359f9d48b231e871f27
Carlos Rodriguez <carlosrodriguez@192.168.1.82>
2025-12-19 16:02:49 -0600

    Cambio de Puerto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     appTitle: "reloj checador",
-    API_ROUTE: "http://144.217.167.101:7676/api/",
+    API_ROUTE: "http://144.217.167.101:3005/api/",
     tableLIMIT: 5
 };
