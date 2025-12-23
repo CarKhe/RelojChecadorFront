2431c2c21427cfa2b64186b8461250f9a232596e
Carlos Rodriguez <carlosrodriguez@192.168.1.96>
2025-12-23 10:45:23 -0600

    fix: https

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     appTitle: "reloj checador",
-    API_ROUTE: "https://crclimasyrefacciones.com/api/" ,
+    API_ROUTE: "https://crclimasyrefacciones.com/api/",
     tableLIMIT: 5
 };
