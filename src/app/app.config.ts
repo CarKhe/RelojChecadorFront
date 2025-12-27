eb3815e22c6ac35dec6cd69cc3aed54e6dd7f8cd
Carlos Rodriguez <carlosrodriguez@192.168.1.149>
2025-12-27 14:34:42 -0600

    Agregando PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
