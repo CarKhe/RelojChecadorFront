f81530178a7dbe96462d1cf260abb657a5a63db4
Carlos Rodriguez <carlosrodriguez@192.168.1.101>
2025-12-29 15:56:50 -0600

    Hard Reload en cada version en todos los dispositivos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('RelojChecadorApp');
+  protected readonly title = signal('Reloj Checador App');
+  
+  constructor(private swUpdate: SwUpdate) {
+    if (swUpdate.isEnabled) {
+      swUpdate.versionUpdates.subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
 }
