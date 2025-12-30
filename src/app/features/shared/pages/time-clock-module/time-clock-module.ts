28ed6d949b2af49f0129c7b1b99de833acd75ae0
Carlos Rodriguez <carlosrodriguez@192.168.1.15>
2025-12-30 08:24:01 -0600

    Area TEST

--- src/app/features/shared/pages/time-clock-module/time-clock-module.ts
+++ src/app/features/shared/pages/time-clock-module/time-clock-module.ts
@@ -132,7 +132,6 @@ export class TimeClockModule implements OnInit, OnDestroy {
         idArea: 0,
         dentroZona: 0
       };
-      alert(JSON.stringify(datos));
       this.timeClockService.enviarDatos(datos).subscribe({
         next: (data) => {
           this.snackBar.success(data.mensaje);
