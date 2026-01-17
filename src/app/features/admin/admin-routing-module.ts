8e37f6c07a7506e27895e8f1b15c7c9fbe0edb3d
Carlos Rodriguez <carlosrodriguez@192.168.1.16>
2026-01-17 14:14:12 -0600

    Modulo Mis Asistencias

--- src/app/features/admin/admin-routing-module.ts
+++ src/app/features/admin/admin-routing-module.ts
@@ -5,6 +5,7 @@ import { AreaModule } from './pages/area-module/area-module';
 import { TimeClockModule } from '../shared/pages/time-clock-module/time-clock-module';
 import { DashboardModule } from './pages/dashboard-module/dashboard-module';
 import { PlantillaModule } from './pages/plantilla-module/plantilla-module';
+import { MisAsistenciasModulo } from '../user/pages/mis-asistencias-modulo/mis-asistencias-modulo';
 
 export const ADMIN_ROUTES: Routes = [
     { 
@@ -15,6 +16,7 @@ export const ADMIN_ROUTES: Routes = [
             { path:'area', component: AreaModule },
             { path:'time', component: TimeClockModule },
             { path:'dashboard', component: DashboardModule },
+            { path: 'misAsistencias', component: MisAsistenciasModulo },
             { path: '**', component: DashboardModule }
         ]
      },
