7f9743043add4519b6df8b21a69d5da5106d4824
Carlos Rodriguez <carlosrodriguez@192.168.1.16>
2026-01-17 14:41:12 -0600

    Adding Table mis Asistencias

--- src/app/features/user/components/tabla-mis-asistencias/tabla-mis-asistencias.ts
+++ src/app/features/user/components/tabla-mis-asistencias/tabla-mis-asistencias.ts
@@ -3,16 +3,20 @@ import { AdminDashboardService } from '../../../../core/services/admin/admin-das
 import { UsuariosUltimosRegistrosDTO } from '../../../../core/DTOs/admin/usuarios-ultimos-registros.dto';
 import { AuthService } from '../../../../core/services/auth/auth-service';
 import { UserAuthDTO } from '../../../../core/DTOs/auth/auth-user.dto';
+import { GenericCard } from "../../../../shared/components/generic-card/generic-card";
+import { GenericTable } from "../../../../shared/components/generic-table/generic-table";
+import { ColumnasDTO } from '../../../../core/DTOs/shared/columnas.dto';
 
 @Component({
   selector: 'app-tabla-mis-asistencias',
-  imports: [],
+  imports: [GenericCard, GenericTable],
   templateUrl: './tabla-mis-asistencias.html',
   styleUrl: './tabla-mis-asistencias.scss',
 })
 export class TablaMisAsistencias implements OnInit {
   usuarios:UsuariosUltimosRegistrosDTO[] = [];
   userData: UserAuthDTO | null = null;
+  columnas:ColumnasDTO[] = [];
 
   constructor( 
     private  serviceDashboard: AdminDashboardService,
@@ -23,6 +27,7 @@ export class TablaMisAsistencias implements OnInit {
     this.userData = this.auth.getUserData();
     if (!this.userData){ this.auth.logout(); return; }
     this.getMyAsistencias(5,this.userData.idUser);
+    this.serviceDashboard.getColumnas().subscribe(data => this.columnas = data);
   }
 
   getMyAsistencias(cant: number, idUser: number){
